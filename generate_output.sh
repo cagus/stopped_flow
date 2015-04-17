e08968d5f27e89b4cb887625518789dbd0cbafca
Björn Dahlgren <bjodah@gmail.com>
2015-04-17 13:53:59 +0200

    Stavfel etc.

--- generate_output.sh
+++ generate_output.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # To run this script you need docker (http://docker.io)
-
+cd $(dirname $0)
 DOCKERIMAGE=./environment # could also be e.g.: 'ubuntu:trusty'
 if [[ "$DOCKERIMAGE" == ./* ]]; then
     DOCKERIMAGE=$(docker build $DOCKERIMAGE | tee /dev/tty | tail -1 | cut -d' ' -f3)
 fi
 docker run -e TERM -e HOST_UID=$(id -u) -e HOST_GID=$(id -g) --net='none' -v $(pwd)/input:/input:ro -v $(pwd)/output:/output -w /input -i $DOCKERIMAGE ./entrypoint.sh
+
