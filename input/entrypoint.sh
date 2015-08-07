080b11d8a83c4f1bc2ab6f6a8ee592a12437f506
Björn Dahlgren <bjodah@gmail.com>
2015-08-07 17:23:21 +0200

    Förbättrat build-script. Liten ordvalsändring.

--- input/entrypoint.sh
+++ input/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 ./build.sh
+BUILD_EXIT=$?
 
 # The docker script is run as root (UID=0, GID=0)
 # the docker environment has the environment variables
 # HOST_UID and HOST_GID set which we may use to change
 # the ownership of the generated output:
 chown -R $HOST_UID:$HOST_GID ../output
+exit $BUILD_EXIT
