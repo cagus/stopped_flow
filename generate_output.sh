12be6cefdc560eb131af99f7e5af379ea084b62c
Björn Dahlgren <bjodah@gmail.com>
2015-08-21 10:01:06 +0200

    Uppdaterade build-scripts

--- generate_output.sh
+++ generate_output.sh
@@ -2,7 +2,7 @@
 # To run this script you need docker (http://docker.io)
 CONT_NAME=kd1080_2015_labbpek
 cd $(dirname $0)
-DOCKERIMAGE=bjodah/bjodah-scicomp
+DOCKERIMAGE=${1:-bjodah/dockerfile-bjodah-scicomp}
 #DOCKERIMAGE=./environment # could also be e.g.: 'ubuntu:trusty'
 if [[ "$DOCKERIMAGE" == ./* ]]; then
     DOCKERIMAGE=$(docker build $DOCKERIMAGE | tee /dev/tty | tail -1 | cut -d' ' -f3)
--- input/entrypoint.sh
+++ input/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-./build.sh
-BUILD_EXIT=$?
-
-# The docker script is run as root (UID=0, GID=0)
+# This docker script is run as root (UID=0, GID=0)
 # the docker environment has the environment variables
 # HOST_UID and HOST_GID set which we may use to change
 # the ownership of the generated output:
-chown -R $HOST_UID:$HOST_GID ../output
+addgroup --gid $HOST_GID mygroup
+adduser --disabled-password --uid $HOST_UID --gid $HOST_GID --gecos '' myuser
+su -m myuser -c ./build.sh
+./build.sh
+BUILD_EXIT=$?
 exit $BUILD_EXIT
